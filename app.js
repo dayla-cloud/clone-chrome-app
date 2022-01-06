2138523d0ac06dd0309c4eb6e12b28bc3d895566
challanstudio <fin.dianekim@gmail.com>
2022-01-06 14:02:56 +0900

    object

---
+++ app.js
@@ -0,0 +1,15 @@
+const player = {
+  name: "sooyeon",
+  points: 30,
+  sex: "female",
+};
+
+console.log(player);
+console.log(player.name);
+
+player.name = "kim";
+
+console.log(player);
+
+player.lastname = "sooyeon";
+console.log(player);
