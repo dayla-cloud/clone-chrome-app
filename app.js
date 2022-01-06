64aca04b9f8d2d0edb61de93555af19e72293695
challanstudio <fin.dianekim@gmail.com>
2022-01-06 14:51:53 +0900

    array object funtion

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
+// array
+
+const days = ["mon", "tue", "wed", "thus", "fri", "sat"];
+console.log(days);
+
+days.push("sun");
+console.log(days);
+
+console.log(days[0], days[3]);
+
+days[3] = "none";
+console.log(days);
+
+days.push("the other");
+console.log(days);
+
+// object | it's looks like CSS
+
 const player = {
   name: "sooyeon",
-  points: 30,
-  sex: "female",
+  age: 30,
 };
 
 console.log(player);
 console.log(player.name);
 
 player.name = "kim";
-
 console.log(player);
 
-player.lastname = "sooyeon";
+player.registerIn = "japan";
 console.log(player);
+
+// function
+
+function plus(a, b) {
+  console.log(a + b);
+}
+
+plus(3, 6);
+
+const buyTo = {
+  vegi: ["tomato", "potato"],
+  fruit: function (e) {
+    console.log("could you buy a " + e + " ?");
+  },
+};
+
+buyTo.fruit(buyTo.vegi[0]);
+
+// recap
+
+const list = {
+  words: ["first name", "last name", "nationality"],
+  quest: function (q) {
+    console.log("What is your " + q + "?");
+  },
+};
+
+list.quest(list.words[0]);
+list.quest(list.words[1]);
+list.quest(list.words[2]);
