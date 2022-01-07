5f725fda80382614745960c00d6e3d87b32d62ae
challanstudio <fin.dianekim@gmail.com>
2022-01-07 15:24:49 +0900

    Difference between className and classList

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const h1s = document.querySelectorAll('h1[class^="title"]');
 
 function handleActive() {
   const colorChanged = "active";
-  if (this.className !== colorChanged) {
-    this.className = colorChanged;
+  if (this.classList.contains(colorChanged)) {
+    this.classList.remove(colorChanged);
   } else {
-    this.className = "";
+    this.classList.add(colorChanged);
   }
   console.log(this.className);
 }
@@ -14,4 +14,7 @@ for (h1 of h1s) {
   h1.addEventListener("mouseover", handleActive);
 }
 
-// raw string 을 사용하지 않도록 항상 변수 또는 함수선언을 하는 것이 좋다.
+// 프로퍼티로서의 className 은 클래스명을 통째로 바꾸었지만 classList 는
+// 기존의 클래스명에 추가,삭제,토글 등의 기능을 한다.
+// classList 의 methos 에 관하여
+// https://developer.mozilla.org/ko/docs/Web/API/Element/classList
