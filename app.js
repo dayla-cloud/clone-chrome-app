86cd9e20d69daef96aadf859d496ebf54b8919ae
challanstudio <fin.dianekim@gmail.com>
2022-01-10 16:53:11 +0900

    3.5 saving username

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Local Storage, the API name when you wanna remember user's inform
+// Saving username
+
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
@@ -7,11 +10,18 @@ const HIDDEN_CLASSNAME = "hidden";
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
+  localStorage.setItem("username", username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
   greeting.classList.remove(HIDDEN_CLASSNAME);
   // greeting.innerHTML = "Hello " + username + "!";
   greeting.innerHTML = `Hello ${username}!`;
   console.log(username);
+  console.dir(username, localStorage);
+}
+
+if (username !== "") {
+  loginForm.classList.toggle(HIDDEN_CLASSNAME);
+  greeting.classList.toggle(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
