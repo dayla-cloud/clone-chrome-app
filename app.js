79ecc4548c4208013dada108e37002413f997c5b
challanstudio <fin.dianekim@gmail.com>
2022-01-10 16:35:43 +0900

    add/remove css hidden

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const h1s = document.querySelectorAll('h1[class^="title"]');
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleActive() {
-  this.classList.toggle("active");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  // greeting.innerHTML = "Hello " + username + "!";
+  greeting.innerHTML = `Hello ${username}!`;
+  console.log(username);
 }
 
-for (h1 of h1s) {
-  h1.addEventListener("mouseover", handleActive);
-}
-
-// 프로퍼티로서의 className 은 클래스명을 통째로 바꾸었지만 classList 는
-// 기존의 클래스명에 추가,삭제,토글 등의 기능을 한다.
-// classList 의 methos 에 관하여
-// https://developer.mozilla.org/ko/docs/Web/API/Element/classList
+loginForm.addEventListener("submit", onLoginSubmit);
