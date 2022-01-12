023802118357aa6ab2e499e210ef5c6319c12550
challanstudio <fin.dianekim@gmail.com>
2022-01-12 14:59:32 +0900

    delete Todo

--- js/todo.js
+++ js/todo.js
@@ -8,6 +8,10 @@ function deleteToDo(event) {
   //   부모태그 지워버리기~~~~
   li.remove();
 }
+// 질문 : const li  는 두 번 명명됐는데 어떻게 따로 실행될 수 있는걸까?
+// 답 : 해당 변수는 자신이 속한 함수가 실행되고 끝날때 만들어지고 사라지므로.
+// 반대로 말하면 함수가 실행되고나면 사라졌으면 하는 변수는 그 안에 넣으면 된다는건가!!?? 개신기
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
