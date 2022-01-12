7710ceb6fba0c47f53b4984ad5a04ba243e01b85
challanstudio <fin.dianekim@gmail.com>
2022-01-12 15:16:15 +0900

    save todos / JSON stringify is so wonderfuuuuul

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,11 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
 function deleteToDo(event) {
   //   console.dir(event.target.parentElement.innerText); 부모태그의 텍스트(newTodo)
   const li = event.target.parentElement;
@@ -25,14 +30,12 @@ function paintToDo(newTodo) {
 }
 
 function handleToDoSubmit(event) {
-  // 엔터시 윈도우 기본값 새로고침 해제
   event.preventDefault();
-  //   Input 의 value값을 newTodo라고 명명
   const newTodo = toDoInput.value;
-  //   새로고침되지 않아 눈에 보이는 Input 에 남은 value값을 비워줌(해당 데이터는 눈에 안보일 뿐 사라지지않음)
   toDoInput.value = "";
-  //   눈에 보이지 않게 처리한 value를 함수 paintToDo 인수에 넣어 꾸며줌
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
